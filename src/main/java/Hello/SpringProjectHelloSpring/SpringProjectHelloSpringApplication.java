1cf424596eeb1e51d145474d80366abd82bba3cf
yibre <cdoyue@gmail.com>
2023-11-07 09:47:13 +0900

    initial commit

---
+++ src/main/java/Hello/SpringProjectHelloSpring/SpringProjectHelloSpringApplication.java
@@ -0,0 +1,13 @@
+package Hello.SpringProjectHelloSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectHelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectHelloSpringApplication.class, args);
+	}
+
+}
