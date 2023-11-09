d5cebbc362bb266010bddf237c968c3d75b76e70
yibre <cdoyue@gmail.com>
2023-11-09 09:19:24 +0900

    [FEAT] 강의 3-2 회원 도메인과 리포지토리 만들기

---
+++ src/main/java/Hello/SpringProjectHelloSpring/controller/HelloController.java
@@ -0,0 +1,13 @@
+package Hello.SpringProjectHelloSpring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!");
+        return "hello";
+    }
+}
---
+++ src/main/java/Hello/SpringProjectHelloSpring/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package Hello.SpringProjectHelloSpring.repository;
+import Hello.SpringProjectHelloSpring.domain.Member;
+import java.util.Optional;
+import java.util.List;
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
