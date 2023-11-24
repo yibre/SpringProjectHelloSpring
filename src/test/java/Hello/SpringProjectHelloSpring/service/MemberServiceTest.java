8044042b309784fdc5ca9c89850feb5221a99312
yibre <cdoyue@gmail.com>
2023-11-24 19:13:18 +0900

    [FEAT] 테스트코드 작성 완료

---
+++ src/test/java/Hello/SpringProjectHelloSpring/service/MemberServiceTest.java
@@ -0,0 +1,20 @@
+package Hello.SpringProjectHelloSpring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    void 회원가입() { // 빌드될때 실제 테스트 코드는
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
