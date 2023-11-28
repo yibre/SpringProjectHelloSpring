9699319138a09d7f8bed005ebb9854d2ab01e094
yibre <cdoyue@gmail.com>
2023-11-28 19:01:11 +0900

    [FEAT] 회원가입 테스트 작성

--- src/test/java/Hello/SpringProjectHelloSpring/service/MemberServiceTest.java
+++ src/test/java/Hello/SpringProjectHelloSpring/service/MemberServiceTest.java
@@ -1,20 +1,46 @@
 package Hello.SpringProjectHelloSpring.service;
 
+import Hello.SpringProjectHelloSpring.domain.Member;
+import Hello.SpringProjectHelloSpring.repository.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class MemberServiceTest {
+    MemberService memberService = new MemberService();
 
     @Test
-    void 회원가입() { // 빌드될때 실제 테스트 코드는
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
     }
 
     @Test
-    void findMembers() {
+    public void 중복_회원_예외() throws IOException {
+        // Given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // When
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));// 예외가 발생해야 함
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
 
-    @Test
-    void findOne() {
-    }
 }
