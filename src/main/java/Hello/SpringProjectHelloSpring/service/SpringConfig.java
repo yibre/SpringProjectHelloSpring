3148841abcc8010d8de2b2f8efeb9af1f3d5cdcd
yibre <cdoyue@gmail.com>
2024-04-21 05:13:37 +0900

    [FEAT] 순수 JDBC
    6. 스프링 DB 접근 기술 > 순수 JDBC
    서버를 껐다 켜도 H2 데이터베이스에 연결되어 데이터가 날아가지 않음

--- src/main/java/Hello/SpringProjectHelloSpring/service/SpringConfig.java
+++ src/main/java/Hello/SpringProjectHelloSpring/service/SpringConfig.java
@@ -1,20 +1,27 @@
 package Hello.SpringProjectHelloSpring.service;
 
+import Hello.SpringProjectHelloSpring.repository.JdbcMemberRepository;
+// import Hello.SpringProjectHelloSpring.repository.JdbcTemplateMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import Hello.SpringProjectHelloSpring.repository.MemberRepository;
 import Hello.SpringProjectHelloSpring.repository.MemoryMemberRepository;
 import Hello.SpringProjectHelloSpring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
-
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
