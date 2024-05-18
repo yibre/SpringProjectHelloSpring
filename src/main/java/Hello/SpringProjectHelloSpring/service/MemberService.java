4e44153877ef87755836034cd6770c80eb1ae090
yibre <cdoyue@gmail.com>
2024-05-18 23:44:50 +0900

    [FEAT] AOP가 필요한 상황

--- src/main/java/Hello/SpringProjectHelloSpring/service/MemberService.java
+++ src/main/java/Hello/SpringProjectHelloSpring/service/MemberService.java
@@ -14,7 +14,6 @@ import java.util.List;
 @Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
-
     // @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -25,9 +24,17 @@ public class MemberService {
 
 
     public Long join(Member member) {
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
@@ -39,7 +46,15 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers "+ timeMs + "ms");
+        }
+
     }
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
